67e0267e82711c987eaa5744b2923c135da87be1
Aditya Yepude <110152094+adityas1102@users.noreply.github.com>
2025-01-20 20:00:26 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        //input
+        for(int i=0; i<=size; i++){
+            numbers[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+
+        // int[] marks = new int[3];
+        // marks[0] = 97;
+        // marks[1] = 94;
+        // marks[2] = 91;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // for(int i=0; i<3; i++){
+        //     System.out.println(marks[i]);
+        // }
+
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==x){ 
+            System.out.println("x found at index:"+i);
+        }
+    }
+
+    }
+}
