70b6cef682166096b4288a53c6d659c25cba1113
Aditya Yepude <110152094+adityas1102@users.noreply.github.com>
2025-02-14 08:47:15 +0530

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,20 +1,10 @@
-package day1a;
+package javaday9abstraction;
 
-import java.util.Scanner;
-
-public class Rectangle {
-
-	public static void main(String[] args) {
-
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter Length");
-		int length=sc.nextInt();
-		
-		System.out.println("Enter Width");
-		int width=sc.nextInt();									//buy
-		
-		int areaRect=length*width;								//cook
-		
-		System.out.println("Area of Rectangle is "+areaRect);   //serve
-	}
+public abstract class Rectangle implements Shape2D
+{
+		@Override
+		public void area()
+		{	
+			System.out.println("L X B");
+		}
 }
