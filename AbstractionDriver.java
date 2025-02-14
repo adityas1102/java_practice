96312ffe759e34fcbf9ad623ebf6b6c511a945e2
Aditya Yepude <110152094+adityas1102@users.noreply.github.com>
2025-02-14 08:48:06 +0530

    Add files via upload

---
+++ AbstractionDriver.java
@@ -0,0 +1,21 @@
+package javaday9abstraction;
+
+public class AbstractionDriver {
+public static void main(String[] args) {
+	Shape2D s;
+	s=new Circle();
+	s.area();
+	s.perimeter();
+	
+	Rectangle r;
+	r=new FootballField();
+	r.area();
+	r.perimeter();
+}
+
+
+		public Shape2D test()
+		{
+			return new FootballField();
+		}
+}
